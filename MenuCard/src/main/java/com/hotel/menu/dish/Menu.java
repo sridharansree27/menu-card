164bce771e25be49a78b94d370590acacfe5765f
sridharansree27 <95526783+sridharansree27@users.noreply.github.com>
2022-07-27 19:44:52 +0530

    hotel menu card project

--- MenuCard/src/main/java/com/hotel/menu/dish/Menu.java
+++ MenuCard/src/main/java/com/hotel/menu/dish/Menu.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
@@ -16,12 +18,12 @@ public class Menu {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel","root","root");
 		return connection;
-	}
+		}
 	
 	public void createTable() throws ClassNotFoundException, SQLException {
 		Statement statement=getConnection().createStatement();
 		statement.executeUpdate("create table menu(dish varchar(20),quantity_in_kg long,price_per_kg long)");
-	}
+		}
 	
 	public void insertDish() throws ClassNotFoundException, SQLException {
 		System.out.println("enter dish name ?");
@@ -34,7 +36,7 @@ public class Menu {
 		PreparedStatement search=getConnection().prepareStatement("select * from menu where dish=?");
 		search.setString(1, dish);		
 		ResultSet rs=search.executeQuery();		
-		if(rs.next()) {
+		if(rs.next()) {			
 			updatePrice(price, rs.getString(1));
 			updateQuantity(quantity+rs.getInt(2), rs.getString(1));
 		}else {
@@ -44,8 +46,8 @@ public class Menu {
 			ps.setLong(3, price);
 			
 			ps.executeUpdate();
+			}
 		}
-	}
 	
 	public void deleteDish(String dish) throws ClassNotFoundException, SQLException {
 		PreparedStatement ps=getConnection().prepareStatement("delete from menu where dish=?");
@@ -53,8 +55,8 @@ public class Menu {
 		
 		if(ps.executeUpdate()==0) {
 			System.out.println("sorry,"+dish+" is already not available !");
+			}
 		}
-	}
 		
 	public void updatePrice(long price,String dish) throws ClassNotFoundException, SQLException {
 		PreparedStatement ps=getConnection().prepareStatement("update menu set price_per_kg=? where dish=?");
@@ -63,7 +65,7 @@ public class Menu {
 		ps.setString(2, dish);
 		
 		ps.executeUpdate();
-	}
+		}
 	
 	public void updateQuantity(int quantity,String dish) throws ClassNotFoundException, SQLException {
 		PreparedStatement ps=getConnection().prepareStatement("update menu set quantity_in_kg=? where dish=?");
@@ -72,66 +74,111 @@ public class Menu {
 		ps.setString(2, dish);
 		
 		ps.executeUpdate();
-	}
+		}
 	
 	public void getDish() throws ClassNotFoundException, SQLException {
+		
+		List<String> orders=new ArrayList<String>();
+		long totalPrice=0;
+		boolean isNext=true;
 		boolean outOfStock=true;
-		PreparedStatement ps=getConnection().prepareStatement("select * from menu where dish=?");
-		System.out.println("enter dish name you want ?");
-		String dish_search=in.next();
-		ps.setString(1, dish_search);
 		
-		System.out.println("enter the quantity in kg ?");
-		int quantity_search=in.nextInt();
+		System.out.println("Do you want to order more true/false ?");
+		isNext=in.nextBoolean();
 		
-		ResultSet rs=ps.executeQuery();
-		while(rs.next()) {
-			outOfStock=false;
-			if(quantity_search<=rs.getInt(2)) {
-			String dish=dish_search;
-			int quantity=quantity_search;
-			long price=quantity*rs.getLong(3);
-			GenerateBill(dish, quantity, price);
-			if(rs.getInt(2)-quantity_search == 0) {
-				deleteDish(rs.getString(1));
-			}else {
-				updateQuantity(rs.getInt(2)-quantity_search,dish);
-			}
-			}else {
-				System.out.println(dish_search+" of "+quantity_search+"kg is not available !");
+		while(isNext) {
+			boolean isPrevious=false;
+		    PreparedStatement ps=getConnection().prepareStatement("select * from menu where dish=?");
+	    	System.out.println("enter dish name you want ?");
+	    	String dish=in.next();
+	    	ps.setString(1, dish);
+		
+		    System.out.println("enter the quantity in kg ?");
+		    int quantity=in.nextInt();
+		
+	    	ResultSet rs=ps.executeQuery();
+		    while(rs.next()) {
+			    outOfStock=false;			    
+			    if(quantity<=rs.getInt(2)) {
+				    if(orders.isEmpty()) {
+				        orders.add(dish);
+				        orders.add(String.valueOf(quantity));
+			        	orders.add(String.valueOf(quantity*rs.getLong(3)));
+			        	totalPrice=totalPrice+quantity*rs.getLong(3);
+			        	}else {
+					        for(String order:orders) {					   
+						        if(dish.equals(order)) {						        	
+							        isPrevious=true;
+						        	orders.set(orders.indexOf(order)+1, String.valueOf(Integer.parseInt(orders.get(orders.indexOf(order)+1)) + quantity ));
+						        	orders.set(orders.indexOf(order)+2, String.valueOf(Integer.parseInt(orders.get(orders.indexOf(order)+2)) + quantity*rs.getLong(3)) );
+						        	totalPrice=totalPrice+quantity*rs.getLong(3);
+						        	break;
+						        	}
+						        }					        
+					        if(!isPrevious) {
+						    orders.add(dish);
+					    	orders.add(String.valueOf(quantity));
+						    orders.add(String.valueOf(quantity*rs.getLong(3)));
+						    totalPrice=totalPrice+quantity*rs.getLong(3);
+					    	if(rs.getInt(2)-quantity == 0) {
+						    	deleteDish(rs.getString(1));
+						    	}else {
+						    		updateQuantity(rs.getInt(2)-quantity,dish);
+						    		}
+					    	break;
+					    	}
+					        }
+				    if(rs.getInt(2)-quantity == 0) {
+				    	deleteDish(rs.getString(1));
+				    	}else {
+				    		updateQuantity(rs.getInt(2)-quantity,dish);
+				    		}
+				    }else {
+				    	System.out.println(dish+" of "+quantity+"kg is not available !");
+				    	}
+			    }
+		    if(outOfStock){
+		    	System.out.println(dish+" is out of stock !");
+		        getMenu();
+		        isNext=false;
+		        }
+		    if(!outOfStock) {
+		    	System.out.println("Do you want to order more true/false ?");
+		    	isNext=in.nextBoolean();
+		    	}
+		    }
+		if(!outOfStock) {
+			GenerateBill(orders,totalPrice);
 			}
 		}
-		
-		if(outOfStock){
-		System.out.println("sorry,"+dish_search+" is out of stock !");
-		getMenu();
-		getDish();
-	}
-	}
 	
 	public void getMenu() throws ClassNotFoundException, SQLException {
-		System.out.println("Available dishes are,");
-		Statement st=getConnection().createStatement();
 		
+		boolean outOfStock=true;
+	
+		Statement st=getConnection().createStatement();		
 		ResultSet rs=st.executeQuery("select * from menu");
-		while(rs.next()) {
-			System.out.println(rs.getString(1)+" "+rs.getInt(2)+" "+rs.getLong(3));
+		
+		if(true) {	
+			while(rs.next()) {
+				outOfStock=false;
+			    System.out.println(rs.getString(1)+" "+rs.getInt(2)+" "+rs.getLong(3));
+			    }
+			if(!outOfStock) {
+				getDish();
+				}else{
+					System.out.println("Sorry, no dishes available now !");
+					}
+			}
 		}
-	}
 	
-	public void orderDish() throws ClassNotFoundException, SQLException {
-		getMenu();
-		getDish();
-	}
-	
-	public void GenerateBill(String dish,int quantity,long price) throws ClassNotFoundException, SQLException {
+	public void GenerateBill(List<String> orders,long total) throws ClassNotFoundException, SQLException {
 		
 		System.out.println("----------------BILL-----------------");
-		System.out.println("DISH: "+dish);
-		System.out.println("QUANTITY: "+quantity);
-		System.out.println("PRICE: "+price);
+		for(int i=0;i<orders.size();++i) {
+			System.out.println("DISH : "+orders.get(i)+" QUANTITY : "+orders.get(++i)+"KG AMOUNT : "+orders.get(++i)+"RUPEE");
+			}
+		System.out.println("TOTAL : "+total+"RUPEE");
 		System.out.println("-------------------------------------");
-		
+		}
 	}
-
-}
--- MenuCard/src/main/java/com/hotel/menu/dish/MenuDriver.java
+++ MenuCard/src/main/java/com/hotel/menu/dish/MenuDriver.java
@@ -11,37 +11,32 @@ public class MenuDriver {
 		
 		boolean exit=false;
 		while(!exit) {
-		System.out.println("------WELCOME TO TOMATO------");
-		System.out.println("1.Create Table \n2.Insert Dish \n3.Order Dish \n4.Get Menu \n5.Exit");
+			System.out.println("------WELCOME TO TOMATO------");
+	    	System.out.println("1.Create Table \n2.Add New Dish \n3.Get Menu \n4.Exit");
 
-		System.out.println("Enter Your's Choice ?");
-		int choice=in.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			menu.createTable();
-			break;
-		}
-		case 2:{
-			menu.insertDish();
-			break;
-		}
-		case 3:{
-			menu.orderDish();
-			break;
-		}
-		case 4:{
-			menu.getMenu();
-			break;
-		}
-		case 5:{
-			exit=true;
-			break;
-		}
-		default:{
-			System.out.println("Invalid Choice Entered !");
-		}
-		}		
-		}	
-	}	
-}
+	    	System.out.println("Enter Your's Choice ?");
+	    	int choice=in.nextInt();
+	    	switch(choice) {
+	    	case 1:{
+	    		menu.createTable();
+	    		break;
+	    		}
+	    	case 2:{
+	    		menu.insertDish();
+	    		break;
+	    		}
+	    	case 3:{
+	    		menu.getMenu();
+	    		break;
+	    		}
+	    	case 4:{
+	    		exit=true;
+	    		break;
+	    		}
+	    	default:{
+	    		System.out.println("Invalid Choice Entered !");
+	    		}
+	        	}		
+	        	}	
+	            }	
+                }
